527c51b0375fc039d812018752cd71fb456c8cf3
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2024-07-18 22:22:22 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
 
 import sys
+import re
 
-# Initialize variables
-total_size = 0
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-try:
-    for idx, line in enumerate(sys.stdin, start=1):
-        # Parse the line
-        parts = line.split()
-        if len(parts) < 7:
-            continue
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-        # Update total file size
-        total_size += file_size
 
-        # Update status code count
-        if status_code in status_code_count:
-            status_code_count[status_code] += 1
+if __name__ == "__main__":
+    regex = re.compile(
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
+    )  # nopep8
 
-        # Check if 10 lines have been processed
-        if idx % 10 == 0:
-            print(f"Total file size: {total_size}")
-            for code in sorted(status_code_count.keys()):
-                if status_code_count[code] > 0:
-                    print(f"{code}: {status_code_count[code]}")
-except KeyboardInterrupt:
-    print(f"Total file size: {total_size}")
-    for code in sorted(status_code_count.keys()):
-        if status_code_count[code] > 0:
-            print(f"{code}: {status_code_count[code]}")
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
+    }
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if match:
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if code.isdecimal():
+                    log["code_frequency"][code] += 1
+
+                if line_count % 10 == 0:
+                    output(log)
+    finally:
+        output(log)
